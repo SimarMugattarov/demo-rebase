da75efc29b14db49e022e169626b4d58b9b4e171
simar mugattarov <simar.mugattarov@quantori.com>
2024-05-17 19:27:41 +0500

    update message featyire2

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         //use system utility to print message
-        //this an app message feature2
+        //this an app message feature2 with new update
         System.out.println(new App().getGreeting());
     }
 }
